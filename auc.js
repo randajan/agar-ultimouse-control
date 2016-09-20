c3cf4d3779d32ef91ae6f23e88ed28888041733a
randajan <jnranda@gmail.com>
2016-09-20 14:56:15 +0200

    Update auc.js

--- auc.js
+++ auc.js
@@ -1,13 +1,19 @@
-// ==UserScript==
-// @name         Agar.io Ultimouse
-// @namespace    AgarUltiMouse
-// @version      2.1
-// @description  Agar.io ultimate mouse control+. Left 1x split, middle infinite split, right infinite feed
-// @author       randajan
-// @match        http://agar.io
-// @grant        none
-// @license      WTFPL v2 (http://www.wtfpl.net/txt/copying/)
-// ==/UserScript==
+/*
+Copyright (C) 2016 Randal
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 var html = {
     d: document, b: function(){return html.d.getElementsByTagName("body")[0];}, h: function() {return html.d.getElementsByTagName("head")[0];},
